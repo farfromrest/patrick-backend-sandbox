1781bd95cecfdf30d78ca5a8c59bdd93d647cac3
Derek Fons <derek@farfromrest.com>
2023-08-16 12:28:23 -0500

    start of login form

---
+++ README.md
@@ -0,0 +1,7 @@
+## TODOs
+
+- Create a login page for email and password
+  x Render a form
+  x Check the form input against users
+  x Show error state for invalid
+  x Show correct authorization state
--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const app = express();
 
+const validUser = {
+  email: "john@doe.com",
+  password: "changeme",
+};
+
+app.set("view engine", "pug");
+
+app.use(express.urlencoded());
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.render("index");
+});
+
+app.post("/login", (req, res) => {
+  if (
+    req.body.email === validUser.email &&
+    req.body.password === validUser.password
+  ) {
+    res.send("yup");
+  } else {
+    res.send("nope");
+  }
 });
 
 app.listen(3000, () => {
